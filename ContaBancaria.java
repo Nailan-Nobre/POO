6ec716dd41528bace90319100e8ecbad617a10ce
Nailan-Nobre <nailannobre22@gmail.com>
2025-04-23 08:50:18 -0300

    ...

---
+++ ContaBancaria.java
@@ -0,0 +1,12 @@
+public class ContaBancaria {
+    private String numeroConta;
+    private String titular;
+    private String nome;
+    private double saldo;
+    public double sacar() {
+        return this.saldo;
+    }
+    public double depositar() {
+        return this.saldo;
+    }
+}
---
+++ Principal.java
@@ -0,0 +1,8 @@
+public class Principal {
+    public static void main(String args[]){
+        ContaBancaria contaBancaria = new ContaBancaria();
+        contaBancaria.numeroConta = "1213y";
+        contaBancaria.titular = "Eu";
+        ContaBancaria.saldo = 300.0;
+    }
+}
